04cfb7a71a67dd3cb479b5629a2d477ac0f427fa
hungtd <hungtd@hblab.vn>
2023-05-30 18:03:56 +0700

    set flag QoS

--- README.md
+++ README.md
@@ -1,11 +1,15 @@
 # mqtt-practice
 
 ## Run publisher
+
 ```
-go run main.go
+go run main.go --qos {qos_level}
 ```
 
 ## Run subscriber
+
+Note: id must be unique number
+
 ```
 go run subscriber/main.go --clientNo {id}
 ```
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"os"
@@ -11,12 +12,19 @@ import (
 	mqtt "github.com/eclipse/paho.mqtt.golang"
 )
 
+var qos *int
+
 var f mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Message) {
 	fmt.Printf("TOPIC: %s\n", msg.Topic())
 	fmt.Printf("MSG: %s\n", msg.Payload())
 }
 
 func main() {
+	// read flag clientNo
+	qos = flag.Int("qos", 0, "QoS")
+	flag.Parse()
+	fmt.Println("Running test with QoS: ", *qos)
+
 	mqtt.DEBUG = log.New(os.Stdout, "", 0)
 	mqtt.ERROR = log.New(os.Stdout, "", 0)
 	opts := mqtt.NewClientOptions().
@@ -44,12 +52,12 @@ func main() {
 
 	count := 0
 	for {
-		time.Sleep(10 * time.Second)
+		time.Sleep(5 * time.Second)
 		fmt.Println("------------------")
 		// Publish a message
 		token := c.Publish(
 			"testtopic/random_shino_19472613",
-			0,
+			byte(*qos),
 			false,
 			fmt.Sprintf("Hello World %d", count),
 		)
